24bffc291b151afa869a282bb44ec2118c5280e6
Suzumiya <2018911711@qq.com>
2023-04-10 08:15:20 +0800

    正在部署SSO

--- src/main/java/suzumiya/constant/MQConstant.java
+++ src/main/java/suzumiya/constant/MQConstant.java
@@ -1,6 +1,5 @@
 package suzumiya.constant;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -17,44 +16,4 @@ public class MQConstant {
     // 监听用户注册接口
     public static final String USER_REGISTER_QUEUE = "user.register.queue";
     public static final String USER_REGISTER_KEY = "user.register";
-    // 监听用户unfollow接口
-    public static final String USER_UNFOLLOW_QUEUE = "user.unfollow.queue";
-    public static final String USER_UNFOLLOW_KEY = "user.unfollow";
-    /* Post */
-    // 监听Post新增接口
-    public static final String POST_INSERT_QUEUE = "post.insert.queue";
-    public static final String POST_INSERT_KEY = "post.insert";
-    // 监听Post删除接口
-    public static final String POST_DELETE_QUEUE = "post.delete.queue";
-    public static final String POST_DELETE_KEY = "post.delete";
-    // 监听Post更新接口
-    public static final String POST_UPDATE_QUEUE = "post.update.queue";
-    public static final String POST_UPDATE_KEY = "post.update";
-    /* Cache */
-    // 更新缓存
-    public static final String CACHE_UPDATE_QUEUE = "cache.update.queue";
-    public static final String CACHE_UPDATE_KEY = "cache.update";
-    // 缓存交换机
-    public static final String CACHE_CLEAR_FANOUT = "cache.clear.fanout";
-    /* Message */
-    // 发送系统消息
-    public static final String MESSAGE_INSERT_QUEUE = "message.insert.queue";
-    public static final String MESSAGE_INSERT_KEY = "message.insert";
-    // 公共聊天室未读数量
-    public static final String MESSAGE_PUBLIC_UNREAD_QUEUE = "message.public.unread.queue";
-    public static final String MESSAGE_PUBLIC_UNREAD_KEY = "message.public.unread";
-    // ws交换机
-    public static final String WS_FANOUT = "ws.fanout";
-    public static String clusterNode;
-    // 删除缓存
-    public static final String CACHE_CLEAR_QUEUE = "cache.clear.queue." + clusterNode;
-    public static final String CACHE_CLEAR_KEY = "cache.clear." + clusterNode;
-    // 发送ws消息
-    public static final String WS_QUEUE = "ws.queue." + clusterNode;
-    public static final String WS_KEY = "ws." + clusterNode;
-
-    @Value("${commons.cluster-node}")
-    public void setClusterNode(String clusterNode) {
-        MQConstant.clusterNode = clusterNode;
-    }
 }
